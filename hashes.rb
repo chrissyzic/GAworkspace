a2d8059c4ef7a3ec1b37350113394f8bee375121
chrissyzic <chrissy.ziccarelli@gmail.com>
2016-01-09 21:32:57 -0500

    added alternate notation for instantiating hashes

--- hashes.rb
+++ hashes.rb
@@ -1,4 +1,5 @@
 person = {"name" => "Chrissy", "gender" => "Over (if you want it to be)", "height" => "63"}
 person_symbols = {:name => "Chrissy", :gender => "Over (if you want it to be)", :height => "63"}
-person_alt = {name: => "Chrissy", gender: => "Over (if you want it to be)", height: => "63"}
+person_alt = {name: "Chrissy", gender: "Over (if you want it to be)", height: "63"} #This notation is only used for declaring a hash, only in newer Ruby versions though!
+
 
